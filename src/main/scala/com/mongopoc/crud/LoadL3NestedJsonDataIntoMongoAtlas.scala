7f0d29407533d58206d07f3350baaeb80e8be563
Hemant <smarthem@gmail.com>
2017-08-10 14:07:52 +0530

    Checked in latest changes.

--- src/main/scala/com/mongopoc/crud/LoadL3NestedJsonDataIntoMongoAtlas.scala
+++ src/main/scala/com/mongopoc/crud/LoadL3NestedJsonDataIntoMongoAtlas.scala
@@ -1,6 +1,6 @@
 package com.mongopoc.crud
 
-import java.time.{Instant, ZoneId}
+import java.time.{Instant, LocalDate, ZoneId}
 import java.time.format.DateTimeFormatter
 
 import org.apache.spark.sql.DataFrame
@@ -11,8 +11,12 @@ import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.WriteConfig
 import com.mongodb.{MongoClient, ServerAddress}
 import com.mongopoc.commons.{MongoConfigurations, SparkSessionProvider}
+import org.apache.hadoop.fs.Path
 import org.apache.spark.broadcast.Broadcast
 import org.bson.Document
+
+import scala.collection.mutable
+import scala.collection.mutable.{ListBuffer => MutableListBuffer}
 
 /**
   * Created by sgar42 on 04-Aug-17.
@@ -21,15 +25,35 @@ object LoadL3NestedJsonDataIntoMongoAtlas extends SparkSessionProvider with Mong
 
 
   def loadL3DataIntoMongoAtlas(baseInputPath : String, startDate : String, numDays : Int, doExpireOldRecords : Boolean) = {
+
     val propertyMap: Map[String, String] = createPropertyMap
-    val inputRawDataDF: DataFrame = createDFFromRawData(baseInputPath, propertyMap)
+    val dateTimeFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd").withZone(ZoneId.systemDefault())
+    val sd = LocalDate.parse(startDate, dateTimeFormat)
+    val inputPathList = mutable.MutableList[String]()
+
+    (0 until  numDays).map{ days =>
+      val date = sd.plusDays(days).format(dateTimeFormat)
+      inputPathList += baseInputPath + Path.SEPARATOR_CHAR + date
+    }
+
+    println("Input Locations for Load L3 Data : ")
+    inputPathList.foreach(println(_))
+
+    /*while (!sd.isAfter(ed)) {
+      inputPathList += baseInputPath + Path.SEPARATOR_CHAR + sd.format(dateTimeFormat)
+      sd = sd.plusDays(1)
+    }*/
+
+    val inputDF = spark.read.json(inputPathList: _*)
+
+    //val inputRawDataDF: DataFrame = createDFFromRawData(baseInputPath, propertyMap)
 
     if (doExpireOldRecords)
-      expireOldVersion(inputRawDataDF, propertyMap)
+      expireOldVersion(inputDF, propertyMap)
 
     val mongoServers = mongo_host.replaceAll(",", ":" + mongo_port) + ":" + mongo_port
     val writeConfig = WriteConfig(Map("uri" -> s"mongodb://$mongoServers/risk.$collection"))
-    MongoSpark.save(inputRawDataDF, writeConfig)
+    MongoSpark.save(inputDF, writeConfig)
 
   }
 
@@ -37,7 +61,7 @@ object LoadL3NestedJsonDataIntoMongoAtlas extends SparkSessionProvider with Mong
     Map(TRADE_ID_BATCH_SIZE -> tradeIdBatchSize, MONGO_COLLECTION -> collection, MONGO_HOST -> mongo_host, MONGO_PORT -> mongo_port, DB_NAME -> "risk", NUM_PARTITIONS -> num_partitions)
   }
 
-  def createDFFromRawData(rawDataLocation: String, propertyMap: Map[String, String]): DataFrame = {
+  /*def createDFFromRawData(rawDataLocation: String, propertyMap: Map[String, String]): DataFrame = {
     val inputDF = spark.read.json(rawDataLocation)
     val resultantDF = inputDF.drop(VALID_TO).drop(VALID_FROM).withColumn(VALID_TO, lit(DEFAULT_VALID_TO_DATE)).withColumn(VALID_FROM, lit(getCurrentTimeStamp()))
 
@@ -48,7 +72,7 @@ object LoadL3NestedJsonDataIntoMongoAtlas extends SparkSessionProvider with Mong
   def getCurrentTimeStamp(): String = {
     val timeStampFormat: String = DateTimeFormatter.ofPattern(TIMESTAMP_FORMAT).withZone(ZoneId.systemDefault()).format(Instant.now)
     timeStampFormat
-  }
+  }*/
 
   def expireOldVersion(dataFrame: DataFrame, propertyMap: Map[String, String]): Unit = {
     import scala.collection.JavaConverters._
