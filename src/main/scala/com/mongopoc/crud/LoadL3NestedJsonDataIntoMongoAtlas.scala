d49eccb745b62fe8161710ff795970c460f44644
Hemant <smarthem@gmail.com>
2017-08-17 15:42:30 +0530

    Checked in changes to make mongo security configurable using application.conf

--- src/main/scala/com/mongopoc/crud/LoadL3NestedJsonDataIntoMongoAtlas.scala
+++ src/main/scala/com/mongopoc/crud/LoadL3NestedJsonDataIntoMongoAtlas.scala
@@ -65,12 +65,10 @@ object LoadL3NestedJsonDataIntoMongoAtlas extends SparkSessionProvider with Mong
     //val inputRawDataDF: DataFrame = createDFFromRawData(baseInputPath, propertyMap)
 
     val mongoServers = mongo_host.replaceAll(",", ":" + mongo_port + ",") + ":" + mongo_port
-    var mongoAtlasURI = ""
-    if(mongoSecurityEnabled)
-      mongoAtlasURI = s"mongodb://$mongoUsername:$mongoPassword@$mongoServers/risk.$collection?ssl=$ssl&replicaSet=$replicaSet&authSource=$authSource"
-    else
-      mongoAtlasURI = s"mongodb://$mongoServers/risk.$collection?ssl=$ssl&replicaSet=$replicaSet"
-
+    val mongoAtlasURI =  if(mongoSecurityEnabled)
+                          s"mongodb://$mongoUsername:$mongoPassword@$mongoServers/risk.$collection?ssl=$ssl&replicaSet=$replicaSet&authSource=$authSource"
+                        else
+                          s"mongodb://$mongoServers/risk.$collection?ssl=$ssl&replicaSet=$replicaSet"
 
     if (doExpireOldRecords)
       expireOldVersion(inputDF, propertyMap, mongoAtlasURI)
