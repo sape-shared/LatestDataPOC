fcb23ea63e521ae5d4a8407c9a9ecb20e26b1b22
Hemant <smarthem@gmail.com>
2017-08-21 16:22:51 +0530

    Checked in changes for output log files.

--- src/main/resources/scripts/QueryMongo/FetchRawData/RunFetchRawData.sh
+++ src/main/resources/scripts/QueryMongo/FetchRawData/RunFetchRawData.sh
@@ -2,14 +2,16 @@ valuationDate=$1
 snapshot=$2
 numThreads=$3
 
+mkdir -p ../../../../output/QueryMongo/FetchOneDayRisk
+chmod 777 ../../../../output/QueryMongo/FetchOneDayRisk
 
 start=$(($(date +%s%N)/1000000))
 for (( i=1; i <= ${numThreads}; ++i ))
 do
-    nohup sh FetchRawData.sh $valuationDate $snapshot FetchRisk_${numThreads}_Concurrent.log > out.log &
+    nohup sh FetchRawData.sh $valuationDate $snapshot ../../../../output/QueryMongo/FetchOneDayRisk/FetchRisk_${numThreads}_Concurrent.log > out.log &
 done
 
 wait
 end=$(($(date +%s%N)/1000000))
 duration=$(($end - $start))
-echo "Query Total Run Time : " ${duration} "milli seconds" >> FetchRisk_${numThreads}_Concurrent.log
+echo "Total Concurrent Run Time : " ${duration} "milli seconds" >> ../../../../output/QueryMongo/FetchOneDayRisk/FetchRisk_${numThreads}_Concurrent.log
