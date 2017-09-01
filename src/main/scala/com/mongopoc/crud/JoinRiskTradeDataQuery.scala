47c3b5255b8eeedf588ea045978beaa63fd8c4ef
Hemant <smarthem@gmail.com>
2017-09-01 08:54:13 +0530

    Changes

--- src/main/scala/com/mongopoc/crud/JoinRiskTradeDataQuery.scala
+++ src/main/scala/com/mongopoc/crud/JoinRiskTradeDataQuery.scala
@@ -1,15 +1,16 @@
 package com.mongopoc.crud
 
 import java.io.File
+import java.sql.Date
+import java.util.Calendar
 
 import com.mongodb.client.model.{Filters => MongoFilters}
 import com.mongodb.{MongoClient, MongoClientURI}
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.ReadConfig
 import com.mongopoc.commons.Constants._
-import com.mongopoc.commons.{MongoConfigurations, SparkSessionProvider}
 import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory.load
+import com.typesafe.config.ConfigFactory._
 import org.apache.spark.sql.SparkSession
 import org.bson.Document
 import org.mongodb.scala.model.Projections
@@ -18,26 +19,29 @@ import scala.collection.JavaConverters._
 
 object JoinRiskTradeDataQuery  extends App{
 
-  val bookIdList: List[String]= List("3553")
+  val bookIdList = List("3553").mkString( "[\"", "\",\"", "\"]")
   val configFilePath = args(0)
   val config: Config = {
-    load(configFilePath)
+    parseFile(new File(configFilePath))
   }
 
   val spark = SparkSession.builder()
     .master(config.getString("configuration.spark.master.url"))
     .appName("Bulk Ingestion Mongo POC")
     .getOrCreate()
-
+  val startTime = Calendar.getInstance().getTimeInMillis
   val riskFindPropertyMap: Map[String, String] = Map(DB_NAME -> "HSBC" , MONGO_COLLECTION -> "riskmeasure")//, "SNAPSHOT"->"20170210120000")
   val mongoDSLUri = "mongodb://localhost:27017"
   val mongoRSLUri = "mongodb://localhost:27017/HSBC.riskmeasure"
 
-  val readDSLConfig = ReadConfig(Map(MONGO_URI->mongoDSLUri, "database"->"HSBC", "collection"->"trades", "partitioner"->"36"))
+  val readDSLConfig = ReadConfig(Map(MONGO_URI->mongoDSLUri, "database"->"HSBC", "collection"->"trades"))
   import spark.implicits._
-  val tradeIdsRDD = MongoSpark.load(spark.sparkContext,readDSLConfig).filter(doc => bookIdList.contains(doc.get[Document]("index", classOf[Document]).getString("tradebookId"))).map(doc => doc.get[Document]("index",classOf[Document]).getString("tradetradeId")).repartition(36)
+  val tempStr = "{ $match: { \"index.booktradeId\" : { $in : ["+ bookIdList +"]  } } }"
+  val tradeIdsRDD = MongoSpark.load(spark.sparkContext,readDSLConfig).withPipeline(Seq(Document.parse("{ $match: { \"index.booktradeId\" : { $in : "+ bookIdList +"  } } }"), Document.parse("{ $project : { \"index.tradetradeId\" : 1} }"))).map(doc => doc.get[Document]("index",classOf[Document]).getString("tradetradeId")).repartition(3)
   val mongoRslURIBr = spark.sparkContext.broadcast(mongoRSLUri)
   val propertyMapBr = spark.sparkContext.broadcast(riskFindPropertyMap)
+  val duration = Calendar.getInstance().getTimeInMillis -startTime
+  println(s"Time taken in fetchin trade ids from DSL : ${duration}" )
 
   val riskData = tradeIdsRDD.mapPartitions{iter =>
     val mongoUri = mongoRslURIBr.value
@@ -45,11 +49,12 @@ object JoinRiskTradeDataQuery  extends App{
     val mongoRSLClient = new MongoClient(new MongoClientURI(mongoUri))
     val collection= mongoRSLClient.getDatabase("HSBC").getCollection("riskmeasure")
 
-    val filter = MongoFilters.and(MongoFilters.in("riskSource.tradeId", iter.toList:_*), MongoFilters.eq("valuationDate", "20170110"))
-    val doc = collection.find(filter).projection(Projections.include("riskSource"))
-    val itr= doc.iterator().asScala
-    mongoRSLClient.close()
-    itr
+    val filter = MongoFilters.and(MongoFilters.in("riskSource.tradeId", iter.toList:_*), MongoFilters.eq("valuationDate", 20170111L))
+    val doc = collection.find(filter)//.projection(Projections.include("riskSource"))
+    doc.iterator().asScala
   }
-  riskData.foreach(x => {})
+  val riskCount = riskData.count()
+  val readRiskDuration = Calendar.getInstance().getTimeInMillis -startTime
+  println(s"Time taken in fetchin risk from from RSL : ${readRiskDuration}" )
+  riskData.foreach(x => println(x))
 }
